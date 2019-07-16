f92b621fbafc6c073fda14eeeb6d2e35abaf31fb
Jaron Schulz <jaronchulz@gmail.com>
2019-07-16 15:24:35 +0200

    Converting stateless to stateless widget with underscore syntax

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,57 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyFirstApp());
 
-class MyFirstApp extends StatelessWidget {
+class MyFirstApp extends StatefulWidget {
+
+@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyFirstAppState();
+  }
+}
+
+class _MyFirstAppState extends State<MyFirstApp> {
+  var _questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+    _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello World!'));
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          backgroundColor: Color.fromARGB(255, 2, 166, 110),
+        ),
+        body: Column(
+          children: [
+            Text(questions[_questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print('Answer 3 chosen!');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
